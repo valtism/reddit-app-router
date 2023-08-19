095b795224dc6b44173ab906f45d7848a54ec4ba
Dan Wood <daniel4wood@icloud.com>
2023-08-18 22:12:29 -0700

    Kick off redeploy with env vars

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "@/db";
 import { CreatePost } from "@/app/CreatePost";
-import { clerkClient, currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 
 export default async function Home() {
   const posts = await prisma.post.findMany();
